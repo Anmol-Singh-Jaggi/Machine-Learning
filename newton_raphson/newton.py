b1be68d53c866a3e38d33aaa1ca0ea1a49cce36c
Anmol Singh Jaggi <anmolj.65@gmail.com>
2016-05-17 15:26:05 +0530

    Maintenance

--- newton_raphson/newton.py
+++ newton_raphson/newton.py
@@ -8,7 +8,10 @@ def Fd(x):
 
 
 def newton(x, iters=10**5, error=10**-4, debug=False):
-    x_old = x + error * 2
+    # Initialize x_old to a random variable such that
+    # [abs(x - x_old) <= error] turns out to be false
+    x_old = x - error - 10
+
     for i in range(iters):
         if debug:
             print(x, F(x), Fd(x))
@@ -16,6 +19,7 @@ def newton(x, iters=10**5, error=10**-4, debug=False):
             break
         x_old = x
         x -= F(x) / Fd(x)
+
     return x
 
 x = newton(100, debug=True)
